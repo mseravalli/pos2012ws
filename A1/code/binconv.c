58367effabe8e0dc8e64df1f4b9b2d24e64e35ef
marco <marco.seravalli@gmail.com>
2012-10-23 15:36:19 +0200

    modified makefile binconv.c added

---
+++ A1/code/binconv.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ( int argc, char** argv ) {
+
+    if ( argc < 2 ) {
+        printf("Usage: ./binconv input_file output_file \n");
+    }
+    
+    //TODO implement everything
+    
+    return 0;
+
+}
