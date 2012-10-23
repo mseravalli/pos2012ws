2cf69d12a127c078f3785e7789e597d153ebd0b1
marco <marco.seravalli@gmail.com>
2012-10-23 23:22:31 +0200

    skeleton for conversion

--- A1/code/binconv.c
+++ A1/code/binconv.c
@@ -1,13 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ *
+ */
 int main ( int argc, char** argv ) {
 
-    if ( argc < 2 ) {
+    if ( argc < 3 ) {
         printf("Usage: ./binconv input_file output_file \n");
+        exit( 1 );
     }
     
-    //TODO implement everything
+    char* in_path = argv[1]; 
+    char* out_path = argv[2];
+
+    FILE* in_file = fopen( in_path, "r" ); 
+    FILE* out_file = fopen( out_path, "w" );
     
+	if ( in_file == NULL ) {
+		printf( "Error opening file: %s\n", in_path );
+		exit( 1 );
+	}
+
+    // read dimensions
+
+    int nintci; 
+    int nintcf; 
+    int nextci; 
+    int nextcf; 
+
+    fscanf( in_file, "%d", &nintci );
+    fscanf( in_file, "%d", &nintcf );
+    fscanf( in_file, "%d", &nextci );
+    fscanf( in_file, "%d", &nextcf );
+
+    int i;
+
+    int temp_int;
+	for ( i = nintci; i <= nintcf; i++ ) {
+		fscanf( in_file, "%d", &temp_int );
+		fscanf( in_file, "%d", &temp_int );
+		fscanf( in_file, "%d", &temp_int );
+		fscanf( in_file, "%d", &temp_int );
+		fscanf( in_file, "%d", &temp_int );
+		fscanf( in_file, "%d", &temp_int );
+	}
+
+    double temp_double;
+	for (i = nintci; i <= nintcf; i++)
+	{
+		fscanf(in_file, "%lf", &temp_double);
+		fscanf(in_file, "%lf", &temp_double);
+		fscanf(in_file, "%lf", &temp_double);
+		fscanf(in_file, "%lf", &temp_double);
+		fscanf(in_file, "%lf", &temp_double);
+		fscanf(in_file, "%lf", &temp_double);
+		fscanf(in_file, "%lf", &temp_double);
+		fscanf(in_file, "%lf", &temp_double);
+	}
+
+	for (i = nintci; i <= nintcf; i++) {
+		fscanf(in_file, "%d", &temp_int);
+    }
+
+//  while ( !feof(in_file) ) {
+//      fprintf( out_file, "%x ", next_int );
+//      fflush( out_file );
+//     
+//      fscanf( in_file, "%d", &next_int );
+//  }
+
+    fclose(in_file);
+    fclose(out_file);
+
     return 0;
 
 }
+
+
+
